14126fed12eea564b55403cf7c031976c076a319
BlureX <junhaoxu13@hotmail.com>
2016-09-09 17:12:01 +1200

    Added application insight and tidied up GUI

--- js/face.js
+++ js/face.js
@@ -1,38 +1,44 @@
 var currentImage = $("#image-file")[0];
 var status_message = $("#loading-message")[0];
+var response = $('#returnValue')[0];
 
 currentImage.addEventListener("change", function () {
-    status_message.innerHTML = "Calculating Age...";
-      sendImage(currentImage.files[0], function () {});
+    response.innerHTML = "Calculating Age...";
+    processImage(function (file){
+      postImage(currentImage.files[0], function () {});
+    })
 });
 
 function readUrl(input) {
     if (input.files) {
         var image_reader = new FileReader();
+        image_reader.onload = function (e) {
+            $("#selected-image").attr("src", e.target.result);
+        };
         image_reader.readAsDataURL(input.files[0]);
     }
 }
 
-// function processImage(callback){
-//   var file = currentImage.files[0];
-//   var reader = new FileReader();
-//   if (file) {
-//     reader.readAsDataURL(file); //used to read the contents of the file
-//   } else {
-//     // status_message.innerHTML = "Invalid file";
-//     // person_name.innerHTML = "Cannot obtain name for this file";
-//     console.log("Invalid file");
-//   }
-//   reader.onloadend = function (e) {
-//     if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
-//       // status_message.innerHTML = "Please upload an image file (jpg or png).";
-//     } else {
-//       callback(file);
-//     }
-//   }
-// }
+function processImage(callback) {
+    var image = currentImage.files[0];
+    var reader = new FileReader();
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+    else {
+        response.innerHTML = "Invalid file";
+    }
+    reader.onloadend = function (e) {
+        if (!image.name.match(/\.(jpg|png|jpeg)$/)) {
+            response.innerHTML = "Image file (jpg, jpeg, png) required.";
+        }
+        else {
+            callback(file);
+        }
+    };
+}
 
-function sendImage(file, callback) {
+function postImage(file, callback) {
     $.ajax({
         url: "https://api.projectoxford.ai/face/v1.0/detect?returnFaceAttributes=age&returnFaceAttributes=age",
         beforeSend: function (xhrObj) {
@@ -46,7 +52,7 @@ function sendImage(file, callback) {
         .done(function (data) {
         if (data) {
             console.log("YES");
-            console.log(data);
+            response.innerHTML = data[0].faceAttributes.age
         }
         else {
 
